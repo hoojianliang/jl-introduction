b6f83a983550415538e310b081e33ebadc20d250
hoojianliang <hoojianliang@users.noreply.github.com>
2022-10-23 14:55:45 +0800

    improve SEO

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,31 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Head from 'next/head';
+import { useTranslation } from "next-i18next";
 
 type ComponentProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: ComponentProps) {
+  const { t } = useTranslation("common");
+
   return (
     <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>{t("head.title")}</title>
+        <link rel="icon" href="/images/profile.jpg" />
+        <meta
+          name="description"
+          content={t("head.description")}
+        />
+        <meta
+          property="og:image"
+          content={`/images/profile.jpg`}
+        />
+        <meta name="og:title" content={t("head.title")} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Navbar />
       <main className="flex-auto container mx-auto">{children}</main>
       <Footer />
